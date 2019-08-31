1a79042d3bbf97215dad1af0a85346c7e650634c
Chakrit Likitkhajorn <chakrit.lj@gmail.com>
2019-08-31 11:17:29 +0700

    Valuate straight and flus

--- src/poker_kata/poker.clj
+++ src/poker_kata/poker.clj
@@ -9,7 +9,7 @@
 
 (defn card-value-number [val]
   (case (string/lower-case val)
-    "a" 1
+    "a" 14
     "j" 11
     "q" 12
     "k" 13
@@ -29,12 +29,15 @@
   (println "Inspect:" v)
   v)
 
+(defn- sort-by-desc [func val]
+  (sort-by func #(compare %2 %1) val))
+
 (defn- highs [card-freq-map]
-  (->> (sort-by val #(compare %2 %1) card-freq-map)
+  (->> (sort-by-desc val card-freq-map)
        (map first)))
 
 (defn valuate-four-card [hand]
-  (let [card-freq-map (->> (map #(:value %) hand)
+  (let [card-freq-map (->> (map :value hand)
                            (frequencies))
         card-freq     (map val card-freq-map)]
     (if (= 4 (apply max card-freq))
@@ -42,9 +45,40 @@
        :highs (highs card-freq-map)}
       nil)))
 
+(defn valuate-flush [hand]
+  (if (= 1 (-> (map :suit hand)
+               (distinct)
+               (count)))
+    {:power :flush
+     :highs (->> (map :value hand)
+                 (sort >))}))
+
+(defn is-vals-straight [hand]
+  (loop [current-hand (rest hand)
+         current-val (first hand)
+         is-straight true]
+    (cond
+      (not is-straight) false
+      (zero?            (count current-hand)) is-straight
+      :else             (and is-straight
+                          (= current-val (+ (first current-hand) 1))
+                          (recur (rest current-hand) (first current-hand) is-straight)))))
+
+
+
+(defn valuate-straight [hand]
+  (let [card-value-list           (->> (map :value hand) (sort >))
+        card-value-list-ace-first (->> (map :value hand)
+                                       (replace {14 1})
+                                       (sort >))]
+    (cond
+      (is-vals-straight card-value-list)            {:power :straight, :highs card-value-list}
+      (is-vals-straight card-value-list-ace-first)  {:power :straight, :highs card-value-list-ace-first}
+      :else                                         nil)))
+
 
 (defn valuate-fullhouse [hand]
-  (let [card-freq-map (->> (map #(:value %) hand)
+  (let [card-freq-map (->> (map :value hand)
                            (frequencies))
         card-freq     (map val card-freq-map)]
     (if (and (some #{3} card-freq) (some #{2} card-freq))
@@ -63,6 +97,8 @@
 (defn hand-power [hand]
   (-> (apply-if-not-nil nil valuate-four-card hand)
       (apply-if-not-nil valuate-fullhouse hand)
+      (apply-if-not-nil valuate-flush hand)
+      (apply-if-not-nil valuate-straight hand)
       (apply-if-not-nil valuate-highcard hand)))
 
 
@@ -81,5 +117,6 @@
   (macroexpand `(def- x 1))
   (ext "10S" 0 (- 3 1))
   (hand-str-to-map "AS")
+  (sort > [1 3 4 8 3 4])
   (let [hand [{:value 3, :suit :h} {:value 3, :suit :s} {:value 3, :suit :d} {:value 3, :suit :c} {:value 7, :suit :c}]]
     (valuate-four-card hand)))
--- test/poker_kata/poker_test.clj
+++ test/poker_kata/poker_test.clj
@@ -24,15 +24,40 @@
    {:value 7 :suit :c}
    {:value 7 :suit :c}])
 (deftest hand-power-test
-  (let [four-card [{:value 3 :suit :h}
-                   {:value 3 :suit :s}
-                   {:value 3 :suit :d}
-                   {:value 3 :suit :c}
-                   {:value 7 :suit :c}]
-        full-house [{:value 3 :suit :h}
-                    {:value 3 :suit :s}
-                    {:value 3 :suit :d}
-                    {:value 7 :suit :c}
-                    {:value 7 :suit :c}]]
+  (let [four-card       [{:value 3 :suit :h}
+                         {:value 3 :suit :s}
+                         {:value 3 :suit :d}
+                         {:value 3 :suit :c}
+                         {:value 7 :suit :c}]
+        straight-flush  [{:value 10 :suit :s}
+                         {:value 11 :suit :s}
+                         {:value 12 :suit :s}
+                         {:value 13 :suit :s}
+                         {:value 14 :suit :s}]
+        straight        [{:value 10 :suit :s}
+                         {:value 11 :suit :d}
+                         {:value 12 :suit :d}
+                         {:value 13 :suit :s}
+                         {:value 14 :suit :s}]
+        straight-2      [{:value 2  :suit :s}
+                         {:value 3  :suit :d}
+                         {:value 4  :suit :d}
+                         {:value 5  :suit :s}
+                         {:value 14 :suit :s}]
+        flush           [{:value 10 :suit :s}
+                         {:value 11 :suit :s}
+                         {:value 3  :suit :s}
+                         {:value 13 :suit :s}
+                         {:value 14 :suit :s}]
+        full-house      [{:value 3 :suit :h}
+                         {:value 3 :suit :s}
+                         {:value 3 :suit :d}
+                         {:value 7 :suit :c}
+                         {:value 7 :suit :c}]]
+
     (is (= (hand-power four-card) {:power :fourcard, :highs [3 7]}))
-    (is (= (hand-power full-house) {:power :fullhouse, :highs [3 7]}))))
+    (is (= (hand-power full-house) {:power :fullhouse, :highs [3 7]}))
+    (is (= (hand-power flush) {:power :flush, :highs [14, 13, 11, 10, 3]}))
+    (is (= (hand-power straight) {:power :straight, :highs [14 13 12 11 10]}))
+    (is (= (hand-power straight-2) {:power :straight, :highs [5 4 3 2 1]}))))
+    ; (is (= (hand-power straight-flush) {:power :fullhouse, :highs [14 13 12 11 10]}))))
